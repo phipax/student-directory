55d10473658f38185926a2e6a62a2120959e10a6
Sundar Venkataraman <phi.pax@gmail.com>
2018-10-02 21:48:34 +0100

    directory.rb 5th version

--- directory.rb
+++ directory.rb
@@ -14,16 +14,8 @@ students = [
 #first we print the list of students
 puts "The students of Villians Academy"
 puts "-------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
+students.each do |x|
+  puts x
+end
 # finally, we print the total number of students
 puts "Overall, we have #{students.count} great students"
