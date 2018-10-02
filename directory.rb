7a736b8c25a0fc45b857af8c8537de7605c6dc30
Sundar Venkataraman <phi.pax@gmail.com>
2018-10-02 21:52:11 +0100

    directory.rb 6th version

--- directory.rb
+++ directory.rb
@@ -11,11 +11,21 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
+def print_header
 #first we print the list of students
-puts "The students of Villians Academy"
-puts "-------------"
-students.each do |x|
-  puts x
+  puts "The students of Villians Academy"
+  puts "-------------"
 end
+def print(names)
+  names.each do |x|
+    puts x
+  end
+end
+def print_footer(names)
 # finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
